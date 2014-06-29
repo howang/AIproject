702a743ec4debf56c165879bf0f24314d02776c6
Hongye Wang <hywangleaf@hotmail.com>
2014-06-29 17:42:30 -0400

    1st

---
+++ src/Method.java
@@ -0,0 +1,104 @@
+import java.util.*;
+/**
+ * Explain detailed the moving strategy.
+ */
+public class Method{
+
+    Queue<Grid> explored;
+    int explored_nodes = 0;
+
+    public void clearExplored() {
+        this.explored = new LinkedList<Grid>();
+    }
+
+    public Method() {
+        explored = new LinkedList<>();
+    }
+
+    public void perform() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void addtoQueue(Grid board) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void possibleMoves(Grid b) {
+        for (int i = 0; i < b.grid.length; i++) {
+            for (int j = 0; j < b.grid[i].length; j++) {
+                Grid tempBoard = new Grid();
+                tempBoard.copy(b.grid);
+                //end of TempBoard
+                switch (b.grid[i][j]) {
+                    case 'B': //tail
+                    case '<':
+                        try {
+                            if (b.grid[i][j - 1] == ' ') {
+                                tempBoard.grid = tempBoard.move(tempBoard.grid, i, j, '<');
+                                if (tempBoard.canAdd(tempBoard.grid, this.explored)) {
+                                    tempBoard.lastGrid = b;
+                                    addtoQueue(tempBoard);
+                                    explored.add(tempBoard);
+                                }
+                            }
+                        } catch (Exception e) {
+                        }
+
+                        break;
+                    case 'A'://head
+                    case '>':
+                        try {
+                            if (b.grid[i][j + 1] == ' ') {
+                                tempBoard.grid = tempBoard.move(tempBoard.grid, i, j, '>');
+                                if (tempBoard.canAdd(tempBoard.grid, this.explored)) {
+                                    tempBoard.lastGrid = b;
+                                    addtoQueue(tempBoard);
+                                    explored.add(tempBoard);
+                                }
+                            }
+                        } catch (Exception e) {
+                        }
+
+                        break;
+                    case '^':
+                        try {
+                            if (b.grid[i - 1][j] == ' ') {
+                                tempBoard.grid = tempBoard.move(tempBoard.grid, i, j, '^');
+                                if (tempBoard.canAdd(tempBoard.grid, this.explored)) {
+                                    tempBoard.lastGrid = b;
+                                    addtoQueue(tempBoard);
+                                    explored.add(tempBoard);
+                                }
+                            }
+                        } catch (Exception e) {
+                        }
+
+                        break;
+                    case 'v':
+                        try {
+                            if (b.grid[i + 1][j] == ' ') {
+                                tempBoard.grid = tempBoard.move(tempBoard.grid, i, j, 'v');
+                                if (tempBoard.canAdd(tempBoard.grid, this.explored)) {
+                                    tempBoard.lastGrid = b;
+                                    addtoQueue(tempBoard);
+                                    explored.add(tempBoard);
+                                }
+                            }
+                        } catch (Exception e) {
+                        }
+
+                        break;
+
+                }
+            }
+        }
+    }
+
+    public boolean isGoal(char[][] board) {
+        if (board[2][5] == 'A') {
+        	System.out.println("25 is "+board[2][5]);
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,37 @@
+/**
+ * Test the efficiency of different methods to reach the final goal
+ */
+public class Test {
+    public static void main(String[] args) {
+    	String method="AStar";//DepthFirst,IterativeDepthFirst,BreathFirst,Greed,AStar
+    	String level="normal";//easy,normal,hard
+    	Grid grid=new Grid(level);
+    	grid.show();
+        System.out.println("l:left; r:right; u:up; d:down; h:head; t:tail");
+        System.out.println("rules:left and right move together, up and down move together, head and tail move together");
+        System.out.println("once the head is move to position[2][5], it wins!");
+    	if (method.equalsIgnoreCase("DepthFirst")) {
+    		DepthFirst method2 = new DepthFirst(grid);
+            method2.perform();
+        }
+    	else if (method.equalsIgnoreCase("IterativeDepthFirst")) {
+    		IterativeDepthFirst method2 = new IterativeDepthFirst(grid);
+            method2.perform();
+        }
+        else if (method.equalsIgnoreCase("BreathFirst")) {
+        	BreathFirst method2 = new BreathFirst(grid);
+            method2.perform();
+        }
+        else if (method.equalsIgnoreCase("Greedy")) {
+            int heuristic = Character.getNumericValue(method.charAt(1));
+            //System.out.println("heuristic is "+heuristic);
+            Greedy method2 = new Greedy(grid, heuristic);
+            method2.perform();
+        }
+        else if (method.equalsIgnoreCase("AStar")) {
+            int heuristic = Character.getNumericValue(method.charAt(1));  
+            AStar method2 = new AStar(grid, heuristic);
+            method2.perform();
+        }
+}
+}
